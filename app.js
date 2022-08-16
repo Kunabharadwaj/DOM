eb206604194922204b6d76fcf66af9961cf160c3
Bharadwaj <kunabharadwaj520@gmail.com>
2022-08-16 12:28:15 +0100

    added source files

---
+++ app.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    let heading = document.createElement('h1');
+    let text = documnet.createTextNode('Document object modelling');
+    heading.appendChild(text);
+    document.body.appendChild(heading);
+}
